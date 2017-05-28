e41e0f56f9dbcd63726374b3f7e7d75d646b2ce3
Sebulon <Sebulon>
2017-05-28 16:51:15 +0200

    ds

--- strandtest.ino
+++ strandtest.ino
@@ -11,16 +11,38 @@
 #define LED_CNT_MID  3
 #define LED_CNT (LED_CNT_RING + LED_CNT_MID)
 #define BOARD_LED 13
+#define BUTTON 2
 
 
 //Global Variabels
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_CNT_RING + LED_CNT_MID, PIN, NEO_GRB + NEO_KHZ800);
-unsigned long millis_cnt = 0; 
-uint16_t pitch = 240;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_CNT_RING, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip_center = Adafruit_NeoPixel(LED_CNT_MID, 5, NEO_GRB + NEO_KHZ800);
+unsigned long millis_cnt_ring = 0; 
+unsigned long millis_cnt_100ms = 0;
+uint16_t pitch = 100;
 
-uint32_t colors[LED_CNT] ={0};
+static FILE uartout = {0};
 
-static FILE uartout = {0} ;
+
+
+enum BUTTON_STATE
+{
+ BUTTON_STATE_RUN = 0,
+ BUTTON_STATE_CONFIG_RING,
+ BUTTON_STATE_GO_TO_CONFIG_RING,
+ BUTTON_STATE_NUM 
+};
+
+enum BUTTON_STATE button_state = BUTTON_STATE_RUN;
+
+
+enum RING_STATE {
+
+  RING_STATE_RED = 0,
+  RING_STATE_GREEN,
+  RING_STATE_BLUE,
+  RING_STATE_NUM
+};
 
 // create a output function
 // This works because Serial.write, although of
@@ -35,14 +57,20 @@ static int uart_putchar (char c, FILE *stream)
 //Init Funktions
 void setup() {
 
+ pinMode(BUTTON, INPUT); 
+
  //Init Strip
  strip.begin();
  strip.show(); // Initialize all pixels to 'off'
 
+ strip_center.begin();
+ strip_center.show(); // Initialize all pixels to 'off'
+
  Serial.begin(9600);
 
  //pinMode(BOARD_LED, OUTPUT);
- millis_cnt = millis();
+ millis_cnt_ring = millis();
+ millis_cnt_100ms = millis();
 
  // fill in the UART file descriptor with pointer to writer.
  fdev_setup_stream (&uartout, uart_putchar, NULL, _FDEV_SETUP_WRITE);
@@ -53,26 +81,100 @@ void setup() {
 }
 
 
+
 void loop() {
 
 
   static uint8_t count =0;
+  uint32_t color;
+
+  //do something all 10mms
+  if(millis() >= millis_cnt_100ms){
+    check_button_state();
+    millis_cnt_100ms += (unsigned long long)100;
+  }
   
 
   //pitch
-  if(millis() >= millis_cnt){
+  if(millis() >= millis_cnt_ring){
 
-    theaterChaseRing(strip.Color(80, 127, 255), 3); // Blue 
-    theaterChaseMid(strip.Color(0, 255, 0), 3); // Blue    
-       
-    millis_cnt += (unsigned long long)pitch;
+    if(  digitalRead(2) )
+    {
+      color = strip.Color(0, 255, 0);
+    }
+    else
+    {
+      color = strip.Color(255, 0, 0);
+    }
+
+      for (uint16_t i=0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i, color);    //turn every third pixel on
+      }
+    strip.show(); 
+    
+
+      for (uint16_t i=0; i < strip_center.numPixels(); i++) {
+        strip_center.setPixelColor(i, color);    //turn every third pixel on
+      }
+    strip_center.show(); 
+    
+    millis_cnt_ring += (unsigned long long)pitch;
   }//end pitch
 
-  for(int i=0; i<LED_CNT; i++)    
-    strip.setPixelColor(i, colors[i]);
-  strip.show();
+
    
 }
+
+
+
+void check_button_state()
+{
+  static uint32_t time_old = 0;
+
+  switch(button_state)
+  {
+
+    case BUTTON_STATE_RUN:
+      if( !digitalRead(BUTTON) ){
+        button_state =  BUTTON_STATE_GO_TO_CONFIG_RING;
+        time_old = millis();
+        printf("Change Button state to: GO RING\n");
+      }
+      break;
+
+    case BUTTON_STATE_GO_TO_CONFIG_RING:
+      if( !digitalRead(BUTTON) ){
+        if((millis() - time_old) >= 5000){
+          button_state =  BUTTON_STATE_CONFIG_RING;  
+          printf("Change Button State to: CONFIG RING\n");        
+        }
+      }
+      else
+      {
+        button_state = BUTTON_STATE_RUN;
+        printf("GOTO RUN\n");
+      }
+      break;
+
+    case BUTTON_STATE_CONFIG_RING:
+        button_state = BUTTON_STATE_RUN;
+        printf("GO FROM CONFIG TO RUN\n");
+      break;
+
+    default:
+        button_state = BUTTON_STATE_RUN;
+        printf("GO FROM default TO RUN");  
+      break;  
+       
+  }
+
+
+
+
+
+  
+}
+
 
 
 //-------------------------| Effects |--------------------------------
@@ -108,28 +210,33 @@ void colorWipe(uint32_t c, uint8_t wait) {
 }
 
 void rainbow(uint8_t wait) {
-  uint16_t i, j;
+  uint16_t i;
+  static uint16_t j =0;
 
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
+  if(j++ >255)
+    j=0;
+
+
+    for(i=0; i<LED_CNT_RING; i++) {
       strip.setPixelColor(i, Wheel((i+j) & 255));
     }
-    strip.show();
-    delay(wait);
-  }
+
 }
 
 // Slightly different, this makes the rainbow equally distributed throughout
 void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
+  uint16_t i;
+  static uint16_t j = 0;
 
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
+  if(j++ > (255 * 5));
+    j=0;
+  
+
+
+    for(i=0; i< 13; i++) {
       strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
     }
-    strip.show();
-    delay(wait);
-  }
+
 }
 
 //Theatre-style crawling lights.
@@ -141,32 +248,36 @@ void theaterChaseRing(uint32_t c, uint8_t chase) {
    q = 0;
 
   for (uint16_t i=0; i < LED_CNT_RING; ) {
-    colors[i+q] = 0;    //turn every third pixel on
+    strip.setPixelColor((i+q), 0);    //turn every third pixel on
     i=i+3;
   }
+
   for (uint16_t i=0; i < LED_CNT_RING;) {
-     colors[i+q] = c;       //turn every third pixel off
-     i=i+3;
+     strip.setPixelColor((i+q), c);       //turn every third pixel off
+     i=i+2;
   }
+
+  
 }
 
 //Theatre-style crawling lights.
 void theaterChaseMid(uint32_t c, uint8_t chase) {
   
-  static int q = 0;
+  static int q , j= 128;
   //calc chase Step
-  if(q++ > chase)
-   q = 0;
+  if(q++ > LED_CNT)
+   q = LED_CNT_RING;
+
+  if(j++ > 255)
+    j =0;
 
   for (uint16_t i=LED_CNT_RING; i < LED_CNT; ) {
-    colors[i+q] = c;    //turn every third pixel on
-    i=i+3;
+    strip.setPixelColor((i),  Wheel((i+j) & 255));    //turn every third pixel on
+   i=i+1;
   }
-  strip.show();
-  for (uint16_t i=LED_CNT_RING; i < LED_CNT;) {
-     colors[i+q], 0;        //turn every third pixel off
-     i=i+3;
-  }
+
+  //strip.setPixelColor((q), 0);        //turn every third pixel off
+ 
 }
 
 
